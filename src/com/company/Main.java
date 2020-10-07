ea0516296a96c490198cf45cdc4b314c4da22b1e
David Ka <david.karnel@student.kyh.se>
2020-10-07 12:18:57 +0200

    Cleaned some booleans up, and renamed ds to dice, and bGameIsOn to gameIsOn.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,36 +4,36 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static Die[] ds;
-    public static boolean bGameIsOn = true;
+    public static Die[] dice;
+    public static boolean gameIsOn = true;
 
     public static void main(String[] args) {
         int iturn = 0;
-        ds = new Die[5];
+        dice = new Die[5];
         for(int d=0;d<5;d++) {
-            ds[d] = new Die();
+            dice[d] = new Die();
         }
         //We will continue until the game is over
-        while(bGameIsOn == true) {
+        while(gameIsOn) {
             iturn = 0;
             System.out.println("Welcome to Yatzi!");
             while(iturn < 3) {
                 System.out.println("Starting turn " + (iturn+1) + " of 3, rolling dice.");
-                for(int i=0;i<ds.length;i++) {
-                    ds[i].DieRoll();
+                for(int i=0;i<dice.length;i++) {
+                    dice[i].DieRoll();
                     //ds[i].value = 5; //Test if yatzi work
-                    System.out.println(i + ": " + ds[i].getString());
+                    System.out.println(i + ": " + dice[i].getString());
                 }
                 //YATZI
                 boolean flag = true;
                 for(int j=1;j<5;j++) {
-                    if(ds[j].value!=ds[j-1].value) {
+                    if(dice[j].value!=dice[j-1].value) {
                         //Set flag to false
                         flag = false;
                     }
                 }
-                if(flag == true) {
-                    System.out.println("You got YATZI! in " + ds[0].value + "'s");
+                if(flag) {
+                    System.out.println("You got YATZI! in " + dice[0].value + "'s");
                     return;
                 } else {
                     //Here we check if there is no Yatzy: then we check what turn we are on and asks the player if we want to continue or not
@@ -43,7 +43,7 @@ public class Main {
                         if(sc.next().equals("y")) {
                             ++iturn;
                         } else {
-                            bGameIsOn = !bGameIsOn;
+                            gameIsOn = !gameIsOn;
                             break;
                         }
                     } else {
@@ -52,7 +52,7 @@ public class Main {
                         if(sc.next().equals("y")) {
                             iturn = 0;
                         } else {
-                            bGameIsOn = !bGameIsOn;
+                            gameIsOn = !gameIsOn;
                             break;
                         }
                     }
--- src/com/company/YatziTest.java
+++ src/com/company/YatziTest.java
@@ -1,4 +1,30 @@
 package com.company;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class YatziTest {
+    @DisplayName("va vill vi här?")
+    @Test
+    void isYatziWhenAllDiceMatches() {
+        Die[] dice = new Die[5];
+        for(Die die: dice) {
+            die.value = 6;
+        }
+        //Assert something?
+    }
+    @DisplayName("Vad ville vi här då?")
+    @Test
+    void isNotYatziWhenOneDieIsNotMatchingTheOther() {
+        Die[] dice = new Die[5];
+        for(Die die: dice) {
+            die.value = 6;
+        }
+        dice[5].value = 1;
+        //Assert something?
+    }
 }
