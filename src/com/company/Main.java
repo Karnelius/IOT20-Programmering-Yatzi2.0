a55061e875570616fff894300c5e571c9e43da6f
David Ka <david.karnel@student.kyh.se>
2020-10-08 22:51:47 +0200

    Working version.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,20 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
+
     public static Die[] dice;
     public static boolean gameIsOn = true;
-    public static int rollingTurn = 0;
+
 
     public static void main(String[] args) {
+        int rollingTurn = 0;
         theDice();
+        welcomeMessage();
         while (gameIsOn) {
-            welcomeMessage();
             while (rollingTurn < 3) {
-                turn();
+                System.out.println("Starting turn " + (rollingTurn + 1) + " of 3, rolling dice.");
                 for (int i = 0; i < dice.length; i++) {
                     dice[i].DieRoll();
                     System.out.println(i + 1 + ": " + dice[i].diceString());
@@ -20,39 +24,31 @@ public class Main {
                     return;
                 } else {
                     if (rollingTurn != 2) {
-                        continueToPlayYesorNo();
+                        System.out.println("Want to throw again? (y for yes, anything else for no)");
+                        ContinueToPlay(rollingTurn++);
                         break;
                     }
                     System.out.println("Game over! Want to play again?");
-                    continueToPlay(rollingTurn = 0);
+                    ContinueToPlay(rollingTurn = 0);
                     break;
                 }
             }
         }
     }
 
-    public static void continueToPlayYesorNo() { // added this to row 23.
-        int rollingTurn = 0;
-        System.out.println("Want to throw again? (y for yes, anything else for no)");
-        continueToPlay(rollingTurn++);
-    }
-
-    public static void turn() {
-        int rollingTurn = 0;
-        System.out.println("Starting turn " + (rollingTurn + 1) + " of 3, rolling dice.");
-    }
-
-    public static void welcomeMessage() {
+    static void welcomeMessage() {
         System.out.println("Welcome to Yatzi");
     }
-    public static void continueToPlay(int i) {
+
+    static void ContinueToPlay(int i) {
         Scanner sc = new Scanner(System.in);
         if (sc.next().equals("y")) {
         } else {
             gameIsOn = !gameIsOn;
         }
     }
-    public static boolean flagIfYatzi() {
+
+    static boolean flagIfYatzi() {
         boolean flagIfYatzi = true;
         for (int j = 1; j < 5; j++) {
             if (dice[j].value != dice[j - 1].value) {
@@ -61,7 +57,8 @@ public class Main {
         }
         return flagIfYatzi;
     }
-    public static void theDice() {
+
+    private static void theDice() {
         dice = new Die[5];
         for (int d = 0; d < 5; d++) {
             dice[d] = new Die();
